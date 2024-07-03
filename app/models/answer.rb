a21e5ccb1d5870003716c5b05380dea1a3a0808c
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-03 13:10:51 +0300

    Исправил Миграции Исправил gitignore Настройки IDE Убрал лишнюю валидацию из Answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validates :correct, inclusion: { in: [true, false] }
 end
 # Answer model
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_012307) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_03_095224) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false
