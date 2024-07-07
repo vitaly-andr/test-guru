5adcf3629199f867dd92a51882380dc0b6fcc0b4
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-07 17:20:27 +0300

    Added Helpers for forms headers year and github link Added links to other resources in project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def form_header(resource)
+    model_name = resource.model_name.human.downcase
+    action = resource.new_record? ? 'Create new' : 'Edit'
+    "#{action} #{model_name}"
+  end
+end
+def current_year
+  Time.now.year
+end
+
+def github_url(author, repo)
+  "https://github.com/#{author}/#{repo}"
 end
