bbcb7ec13162d4eff9220d10894c467c137888f0
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-09 20:44:25 +0300

    TestPassagesHelper refactoring

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,15 +1,15 @@
 module TestPassagesHelper
   def test_result_message(test_passage)
     success_percentage = test_passage.success_percentage
-    success = test_passage.success?
 
-    message = if success
-                "Congratulations! You passed the test with #{success_percentage.round}% success!"
-              else
-                "Unfortunately, you did not pass the test. Your success rate is #{success_percentage.round}%."
-              end
+    if test_passage.success?
+      message = "Congratulations! You passed the test with #{success_percentage.round}% success!"
+      color = "green"
+    else
+      message = "Unfortunately, you did not pass the test. Your success rate is #{success_percentage.round}%."
+      color = "red"
+    end
 
-    color = success ? "green" : "red"
     content_tag :span, message, style: "color: #{color};"
   end
 end
