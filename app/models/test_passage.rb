a8be46ed9af17f7df591cb521daab88e9a20cc7e
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-09 11:20:10 +0300

    Сделал установку вопроса через один колбэк

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -14,8 +14,7 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
-  before_validation :before_validation_set_first_question, on: :create
-  before_save :set_next_question, if: :persisted?
+  before_save :set_question
 
   def completed?
     current_question.nil?
@@ -46,10 +45,13 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def set_question
+    if new_record?
+      self.current_question = test.questions.first if test.present?
+    else
+      self.current_question = next_question
+    end
   end
-
 
   def correct_answer?(answer_ids)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
@@ -63,7 +65,4 @@ class TestPassage < ApplicationRecord
     test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
-  def set_next_question
-    self.current_question = next_question
-  end
 end
