36f9e5c0f6a7eb5df14dd99ef6ede966eefd3450
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-21 13:48:41 +0300

    changed db:reset

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,6 +11,13 @@ bundle exec rake assets:precompile
 # Clean assets with trace
 bundle exec rake assets:clean
 
-bundle exec rake db:reset
+# Reset database if it's the first deployment
+if [ "$RAILS_ENV" == "production" ]; then
+  echo "Resetting database..."
+  DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset
+else
+  echo "Migrating database..."
+  bundle exec rake db:migrate
+fi
 
 
