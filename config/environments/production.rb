992c3c9fbd0a952c072bda03330a61c3e76a1dfb
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-21 13:42:02 +0300

    added config.hosts for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << 'test-guru.andrianoff.online'
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
