b3851f8b0261f8e7c0a45c55f89df2962fd14e2a
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-23 20:32:48 +0300

    Создал контроллер для обратной связи

---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,21 @@
+class FeedbacksController < ApplicationController
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      FeedbackMailer.send_feedback(@feedback).deliver_now
+      redirect_to root_path, notice: 'Your feedback was successfully sent.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:name, :email, :message)
+  end
+end
