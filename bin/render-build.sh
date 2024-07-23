ed22f974d41daf8af201edc7459dd516d2b9c028
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-23 19:21:56 +0300

    db reset

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -13,17 +13,17 @@ function db_exists() {
   bundle exec rails db:exists
   return $? # Capture the exit status of the rake task
 }
-
+rails db:reset
 # Check if the database exists
-if db_exists; then
-  echo "Database exists. Running migrations..."
-  bundle exec rake db:migrate
-else
-  echo "Database does not exist. Creating and seeding..."
-  bundle exec rake db:create
-  bundle exec rake db:migrate
-  bundle exec rake db:seed
-fi
+#if db_exists; then
+#  echo "Database exists. Running migrations..."
+#  bundle exec rake db:migrate
+#else
+#  echo "Database does not exist. Creating and seeding..."
+#  bundle exec rake db:create
+#  bundle exec rake db:migrate
+#  bundle exec rake db:seed
+#fi
 
 if [ "$RAILS_ENV" = "production_local" ]; then
   echo "Starting server in ${RAILS_ENV} environment..."
