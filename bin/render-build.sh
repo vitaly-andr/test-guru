e71c94ab5f76b0193b8bd19b10e2f390a716bc7e
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-07-23 20:57:58 +0300

    Исправил скрипт deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -13,18 +13,18 @@ function db_exists() {
   bundle exec rails db:exists
   return $? # Capture the exit status of the rake task
 }
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
 
 # Check if the database exists
-#if db_exists; then
-#  echo "Database exists. Running migrations..."
-#  bundle exec rake db:migrate
-#else
-#  echo "Database does not exist. Creating and seeding..."
-#  bundle exec rake db:create
-#  bundle exec rake db:migrate
-#  bundle exec rake db:seed
-#fi
+if db_exists; then
+  echo "Database exists. Running migrations..."
+  bundle exec rake db:migrate
+else
+  echo "Database does not exist. Creating and seeding..."
+  bundle exec rake db:create
+  bundle exec rake db:migrate
+  bundle exec rake db:seed
+fi
 
 if [ "$RAILS_ENV" = "production_local" ]; then
   echo "Starting server in ${RAILS_ENV} environment..."
